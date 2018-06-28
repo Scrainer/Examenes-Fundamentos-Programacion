7435f191eb55f6c5610ba9922970a121f244c556
César Carlier <30296718+Scrainer@users.noreply.github.com>
2018-06-28 10:52:59 -0500

    Delete pr.py

--- 2017-2S/1er Parcial/pr.py
+++
@@ -1,8 +0,0 @@
-numero=[]
-articulos=["el","las","los","la"]
-a=input("ingrese una frase:")
-b=a.split(" ")
-for i in b:
-    if i in articulos:
-        numero.append(articulos)
-print(len(numero))
