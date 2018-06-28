cceacd1520f43edc8121caf3ed408ed1b67f99fe
César Carlier <30296718+Scrainer@users.noreply.github.com>
2018-06-28 10:57:54 -0500

    Add files via upload

---
+++ 2017-2S/1er Parcial/Tema 4.py
@@ -0,0 +1,18 @@
+f = ["a","c","z","m","k"]
+g = [3,4,5,6,5,7]
+t = ""
+for c in f:
+    a = f.index(c)
+    b= g[:a]
+    t = t + (c*len(b))
+print(t)
+
+#------------------------------------------
+
+import numpy as np
+vector = np.array([1,5,6,6,5,2,1,3,7,9,0,0,1,4,8])
+print(vector[vector % 2 == 0])
+print(np.unique(vector[vector % 2 == 0]))
+
+
+print(np.unique(vector[vector % 2 == 0]).size)
